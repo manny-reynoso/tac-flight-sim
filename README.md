7180e957947839efb872325dc85e5dd2316d9c3b
manny-reynoso <manny.d.reynoso@gmail.com>
2026-08-25 00:57:41 -0500

    Add: build Filed and updated ReadMe.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Aerospace-Simulator
+
+
+
+build and run the project
+
+cmake -B build
+cmake --build build
+./build/tac-flight-sim
--- build/CMakeFiles/tac-sim-flight.dir/src/main.cpp.o.d
+++ build/CMakeFiles/tac-sim-flight.dir/src/main.cpp.o.d
@@ -1,2 +1,4 @@
 CMakeFiles/tac-sim-flight.dir/src/main.cpp.o: \
- /home/manny/code/tac-flight-sim/src/main.cpp /usr/include/stdc-predef.h
+ /home/manny/code/tac-flight-sim/src/main.cpp /usr/include/stdc-predef.h \
+ /home/manny/code/tac-flight-sim/build/_deps/raylib-src/src/raylib.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16/include/stdarg.h
