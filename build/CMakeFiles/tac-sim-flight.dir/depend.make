87e4a723cc38b110f3d01458f3f82a156e002e96
manny-reynoso <manny.d.reynoso@gmail.com>
2026-08-25 11:23:06 -0500

    Untrack build/ directory (already covered by .gitignore)
    build/ was committed before the .gitignore rule was added; this
    removes it from tracking without touching the local build output.

--- build/CMakeFiles/tac-sim-flight.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tac-sim-flight.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/tac-sim-flight.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/tac-sim-flight.dir/src/main.cpp.o: \
- /home/manny/code/tac-flight-sim/src/main.cpp /usr/include/stdc-predef.h \
- /home/manny/code/tac-flight-sim/build/_deps/raylib-src/src/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/16/include/stdarg.h
