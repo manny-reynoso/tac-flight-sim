311999bbedffd6abd8cf981fff4918a9f2943969
manny-reynoso <manny.d.reynoso@gmail.com>
2026-08-25 11:44:35 -0500

    Add: 3D Camera

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,18 @@
 int main() {
   int screenWidth{1280};
   int screenHeight{720};
-
   InitWindow(screenWidth, screenHeight, "tac-flight-sim");
-  SetTargetFPS(60);
+
+  // 3D Camera
+
+  Camera3D camera = {0};
+  camera.position = (Vector3){0.0f, 10.0f, 10.0f}; // camera postion
+  camera.target = (Vector3){0.0f, 0.0f, 0.0f};     // Camera looks at point
+  camera.up = (Vector3){0.0f, 0.0f, 0.0f};
+  camera.fovy = 45.0f;                    // Camera FOV Y tac-flight-sim
+  camera.projection = CAMERA_PERSPECTIVE; // Camera mode type
+
+  SetTargetFPS(60); // Sets Target Frames per second
 
   while (!WindowShouldClose()) {
     BeginDrawing();
